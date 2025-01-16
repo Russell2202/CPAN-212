427c6ab6f3b55c31a7158bd296dbfea54eec4631
Russell2202 <n01426507@humbermail.ca>
2025-01-15 22:43:27 -0500

    Reinitialize repository

--- labs/lab1/holiday_list.js
+++ labs/lab1/holiday_list.js
@@ -1,7 +1,7 @@
 //Include Lodash
 const _ = require('lodash'); 
 
-//Creation of list of holidays
+//Creation of list of holidays (Question 1 )
 //Need name and date property for loop
 const holidays = [
     { name: "Christmas", date: "2025-12-25" },
@@ -11,25 +11,24 @@ const holidays = [
     { name: "Halloween", date: "2025-10-31"}
   ];
 
-  const randomHoliday = _.sample(holidays);
-  console.log(`Random holiday: ${randomHoliday.name} is on ${randomHoliday.date}.`);
+//Set variable Today to accurate current date
+const today = new Date();
+//Loop for each holiday (5 times)
+holidays.forEach(holiday => {
+//Calculate how many days till holiday holiday date - current date divide by MS to get days
+console.log(`${holiday.name} is in ${Math.ceil((new Date(holiday.date) - today) / 86400000)} days.`);
+
+//Choses one of the created holidays and lists the date (Question 3)
+const randomHoliday = _.sample(holidays);
+console.log(`Random holiday: ${randomHoliday.name} is on ${randomHoliday.date}.`);
  
 
-  // Find index of Christmas
+// Find index position of Christmas (Question 4)
 const christmasIndex = _.findIndex(holidays, { name: "Christmas" });
-
-// Find index of Canada Day
-const canadaDayIndex = _.findIndex(holidays, { name: "Canada Day" });
-
 console.log(`Index of Christmas: ${christmasIndex}`);
-console.log(`Index of Canada Day: ${canadaDayIndex}`);
 
-  //Set variable Today to accurate current date
-  const today = new Date();
-  //Loop for each holiday (5 times)
-  holidays.forEach(holiday => {
-  //Calculate how many days till holiday holiday date - current date 
-  // div by MS to get days
-    console.log(`${holiday.name} is in ${Math.ceil((new Date(holiday.date) - today) / 86400000)} days.`);
-  });
+// Find index position of Canada Day (Question 4)
+const canadaDayIndex = _.findIndex(holidays, { name: "Canada Day" });
+console.log(`Index of Canada Day: ${canadaDayIndex}`);
+});
     
