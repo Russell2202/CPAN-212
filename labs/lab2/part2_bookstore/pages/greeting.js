9b40b20b2d71d4d82f6278211e2276805121dc5c
Russell2202 <n01426507@humbermail.ca>
2025-01-23 15:24:10 -0500

    Lab 2 - Start

---
+++ labs/lab2/part2_bookstore/pages/greeting.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+app.get('/', (req, res) => {
+    res.send('Hello Russell Fenton, Student ID: n01426507')
+  })
+
+module.exports = router;
